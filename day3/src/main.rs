49950b3847b96b0cd95d64dd6edb4d65b8d8e44d
Santeri Salmijärvi <santeri.salmijarvi@aalto.fi>
2017-12-03 22:59:46 +0200

    Add solution to 2nd star of day 3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,137 @@
+// https://adventofcode.com/2017/day/3
+
 const INPUT: i32 = 312051;
+
+fn main() {
+    // First star
+    let mut stride_len = 1;
+    let mut remaining = INPUT;
+    let mut x: i32 = 0;
+    let mut y: i32 = 0;
+    let mut dir = Direction::Right;
+
+    // Go through spiral with full "strides", length increases by one in two corners
+    while remaining - stride_len > 0 {
+        remaining -= stride_len;
+        match dir {
+            Direction::Up => {
+                y += stride_len;
+                dir = Direction::Left;
+                stride_len += 1;
+            }
+            Direction::Left => {
+                x -= stride_len;
+                dir = Direction::Down;
+            }
+            Direction::Down => {
+                y -= stride_len;
+                dir = Direction::Right;
+                stride_len += 1;
+            }
+            Direction::Right => {
+                x += stride_len;
+                dir = Direction::Up;
+            }
+        }
+    }
+
+    // Take remaining steps
+    match dir {
+        Direction::Up => y += remaining - 1,
+        Direction::Left => x -= remaining - 1,
+        Direction::Down => y -= remaining - 1,
+        Direction::Right => x += remaining - 1,
+    }
+
+    println!(
+        "Square located at x:{} y:{}, shortest path to start is {}",
+        x,
+        y,
+        x.abs() + y.abs()
+    );
+
+    // Second star
+    let mut stride_len = 1;
+    let mut steps = 0;
+    let mut current = 1;
+    let mut x: i32 = 1;
+    let mut y: i32 = 0;
+    let mut dir = Direction::Up;
+    let mut sums: Vec<i32> = Vec::new();
+    sums.push(1);
+
+    // Take single steps from first square
+    while *sums.last().expect("Empty vector") <= INPUT {
+        // Get neighbors
+        let neighbors = match dir {
+            Direction::Up => [
+                get_tile_index(x, y - 1),
+                get_tile_index(x - 1, y - 1),
+                get_tile_index(x - 1, y),
+                get_tile_index(x - 1, y + 1),
+            ],
+            Direction::Left => [
+                get_tile_index(x + 1, y),
+                get_tile_index(x - 1, y - 1),
+                get_tile_index(x, y - 1),
+                get_tile_index(x + 1, y - 1),
+            ],
+            Direction::Down => [
+                get_tile_index(x, y + 1),
+                get_tile_index(x + 1, y + 1),
+                get_tile_index(x + 1, y),
+                get_tile_index(x + 1, y - 1),
+            ],
+            Direction::Right => [
+                get_tile_index(x - 1, y),
+                get_tile_index(x - 1, y + 1),
+                get_tile_index(x, y + 1),
+                get_tile_index(x + 1, y + 1),
+            ],
+        };
+
+        // Sum valid neighbors
+        let mut sum: i32 = 0;
+        for i in 0..4 {
+            if neighbors[i] < current {
+                sum += sums[neighbors[i] as usize];
+            }
+        }
+        sums.push(sum);
+
+        // Step
+        steps += 1;
+        current += 1;
+        match dir {
+            Direction::Up => y += 1,
+            Direction::Left => x -= 1,
+            Direction::Down => y -= 1,
+            Direction::Right => x += 1,
+        }
+
+        // Handle corners
+        if steps >= stride_len {
+            match dir {
+                Direction::Up => {
+                    dir = Direction::Left;
+                    stride_len += 1;
+                }
+                Direction::Left => dir = Direction::Down,
+                Direction::Down => {
+                    dir = Direction::Right;
+                    stride_len += 1;
+                }
+                Direction::Right => dir = Direction::Up,
+            }
+            steps = 0;
+        }
+    }
+
+    println!(
+        "Next written value would be {}",
+        sums.last().expect("Sums empty")
+    );
+}
 
 enum Direction {
     Up,
@@ -7,45 +140,18 @@ enum Direction {
     Right,
 }
 
-fn main() {
-    let mut step = 1;
-    let mut remaining = INPUT;
-    let mut x: i32 = 0;
-    let mut y: i32 = 0;
-    let mut dir = Direction::Right;
-    while remaining - step > 0 {
-        remaining -= step;
-        match dir {
-            Direction::Up => {
-                y += step;
-                dir = Direction::Left;
-                step += 1;
-            }
-            Direction::Left => {
-                x -= step;
-                dir = Direction::Down;
-            }
-            Direction::Down => {
-                y -= step;
-                dir = Direction::Right;
-                step += 1;
-            }
-            Direction::Right => {
-                x += step;
-                dir = Direction::Up;
-            }
+fn get_tile_index(x: i32, y: i32) -> i32 {
+    if y * y >= x * x {
+        if y < x {
+            4 * y * y - y - x - 2 * (y - x)
+        } else {
+            4 * y * y - y - x
+        }
+    } else {
+        if y < x {
+            4 * x * x - y - x + 2 * (y - x)
+        } else {
+            4 * x * x - y - x
         }
     }
-    match dir {
-        Direction::Up => y += remaining - 1,
-        Direction::Left => x -= remaining - 1,
-        Direction::Down => y -= remaining - 1,
-        Direction::Right => x += remaining - 1,
-    }
-    println!(
-        "Square located at x:{} y:{}, shortest path to start is {}",
-        x,
-        y,
-        x.abs() + y.abs()
-    );
 }
